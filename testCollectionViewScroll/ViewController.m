78bde26d72f06ce70f5150ac8944d7490870fdb2
lequysang <lequysang84@gmail.com>
2013-02-04 16:45:55 +0700

    CollectionViewImages

---
+++ testCollectionViewScroll/ViewController.m
@@ -0,0 +1,41 @@
+//
+//  ViewController.m
+//  testCollectionViewScroll
+//
+//  Created by Quy Sang Le on 2/2/13.
+//  Copyright (c) 2013 Quy Sang Le. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
+    return 8;
+}
+
+// The cell that is returned must be retrieved from a call to -dequeueReusableCellWithReuseIdentifier:forIndexPath:
+- (MyCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
+    MyCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"CELL" forIndexPath:indexPath];
+    cell.imageView.image = [UIImage imageNamed:[NSString stringWithFormat:@"flower%i",indexPath.row]];
+    return cell;
+    
+}
+
+
+@end
